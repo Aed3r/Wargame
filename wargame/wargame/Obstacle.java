912c90114fe297f18b1297bebecf84d2d2ceab56
Marwan <marwan@pop-os.localdomain>
2020-11-25 15:44:33 +0100

    Mise en place des fichiers de base

--- wargame/wargame/Obstacle.java
+++ wargame/wargame/Obstacle.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 public class Obstacle extends Element {
 	public enum TypeObstacle {
-		ROCHER (COULEUR_ROCHER), FORET (COULEUR_FORET), EAU (COULEUR_EAU);
+		ROCHER (Color.BLACK), FORET (Color.GREEN), EAU (Color.BLUE);
 		private final Color COULEUR;
 		TypeObstacle(Color couleur) { COULEUR = couleur; }
 		public static TypeObstacle getObstacleAlea() {
