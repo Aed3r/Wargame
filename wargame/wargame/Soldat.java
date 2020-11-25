d6bcdcd0b16a27ab784c4a4c02f13ba288ddadc2
Marwan <marwan@pop-os.localdomain>
2020-11-25 19:31:42 +0100

    feat : Classe abstraite Soldat

--- wargame/wargame/Soldat.java
+++ wargame/wargame/Soldat.java
@@ -1,5 +1,24 @@
 package wargame;
 
-public class Soldat {
-    /*A faire*/
+public abstract class Soldat extends Element implements ISoldat{
+    private final int POINT_DE_VIE_MAX, PORTEE_VISUELE, PUISSANCE, TIR; 
+    private int pointsDeVie;
+    private Carte carte;
+    private Position pos;
+
+    Soldat(Carte carte, int pts, int portee, int puiss, int tir, position pos){
+        this.POINT_DE_VIE_MAX = this.pointsDeVie = pts;
+        this.PORTEE_VISUELE = portee;
+        this.PUISSANCE = puiss;
+        this.TIR = tir;
+        this.carte = carte; this.pos = pos;
+    }
+
+    @Override
+    public abstract int getPoints(); 
+    public abstract int getTour(); 
+    public abstract int getPortee();
+    public abstract void joueTour(int tour);
+    public abstract void combat(Soldat soldat);
+    public abstract void seDeplace(Position newPos);
 }
