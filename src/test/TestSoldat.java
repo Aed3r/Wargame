2e31feb7db15a8652c10087748fb298f72141dfc
Marwan <marwan@pop-os.localdomain>
2020-11-27 15:03:45 +0100

    test : ajout du test pour les soldats (combat au corps a corps) encore

---
+++ src/test/TestSoldat.java
@@ -0,0 +1,25 @@
+package test;
+
+import terrains.Carte;
+import unites.*;
+import wargame.ISoldat.TypesH;
+
+public class TestSoldat {
+    public static void main(String[] args) {
+        Carte c = new Carte();
+        Heros humain = new Heros(c, TypesH.HUMAIN, "Bernard", new Position(19, 7)); 
+        Heros nain = new Heros(c, TypesH.NAIN, "Blblbl", new Position(18, 5));
+        Heros hobbit = new Heros(c, TypesH.HOBBIT, "Frodon", new Position(20, 7));  
+        
+        System.out.println("Soldats sur le terrain :");
+        System.out.println(humain.toString());
+        System.out.println(nain.toString());
+        System.out.println(hobbit.toString());
+
+        humain.combat(hobbit);
+
+        System.out.println("L'humain et le hobbit se battent");
+        System.out.println(humain.toString());
+        System.out.println(hobbit.toString());
+    }
+}
